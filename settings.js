d8f1a08ce85f1a36c78798253a2020e2612695f5
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2022-04-30 06:42:15 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,19 +1,18 @@
 let markets = [
 
     {
-        marketName: 'newdex',
-        URL: 'https://api.newdex.io/v1/tickers',
+        marketName: 'bittrex',
+        URL: 'https://bittrex.com/api/v1.1/public/getmarketsummaries',
         toBTCURL: false,
         pairURL : '',
         last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
             return new Promise(function (res, rej) {
-		const json = res.json();
-		    try {
-                    for (let obj of json.data.symbol) {
-                        if(obj["symbol"].includes(q => q.symbol === 'eosio.token-wax-eos')) {
-                            let coinName = obj["symbol"].replace(q => q.symbol === 'eosio.token-wax-eos', '');
+                try {
+                    for (let obj of data.result) {
+                        if(obj["MarketName"].includes('BTC-')) {
+                            let coinName = obj["MarketName"].replace("BTC-", '');
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].newdex = obj.last;
+                            coin_prices[coinName].bittrex = obj.Last;
                         }
                     }
                     res(coin_prices);
@@ -29,19 +28,18 @@ let markets = [
     },
 
     {
-        marketName: 'alcor',
-        URL: 'https://eos.alcor.exchange/api/markets',
+        marketName: 'poloniex',
+        URL: 'https://poloniex.com/public?command=returnTicker',
         toBTCURL: false,
         pairURL : '',
         last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
             return new Promise(function (res, rej) {
-		const json = res.json();
-		    try {
-                    for (let obj of json.data.symbol) {
-                        if(obj["name"].includes(q => q.symbol.name === 'EOS')) {
-                            let coinName = obj["name"].replace(q => q.symbol.name === 'EOS', '');
+                try {
+                    for (var obj in data) {
+                        if(obj.includes('BTC_')&&obj!=="BTC_EMC2") {
+                            let coinName = obj.replace("BTC_", '');
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].alcor = obj.last_price;
+                            coin_prices[coinName].poloniex = data[obj].last;
                         }
                     }
                     res(coin_prices);
@@ -60,15 +58,10 @@ let markets = [
 
 let marketNames = [];
 for(let i = 0; i < markets.length; i++) { // Loop except cryptowatch
-	marketNames.push([[markets[i].marketName], [markets[i].pairURL]]);
+    marketNames.push([[markets[i].marketName], [markets[i].pairURL]]);
 }
-
 console.log("Markets:", marketNames);
-
 module.exports = function () {
-
-	this.markets = markets;
-    
-	this.marketNames = marketNames;
-
+    this.markets = markets;
+    this.marketNames = marketNames;
 };
