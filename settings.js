ccdea95d9662b85a796c4c9736e6b9676bdcdb01
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2022-05-01 15:55:05 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,18 +1,80 @@
+//
+// let boilerPlateMarket =
+// {
+//     marketName: '',
+//     URL: '', //URL To Fetch API From.
+//     toBTCURL: false, //URL, if needed for an external bitcoin price api.
+//     last: function (data, coin_prices) { //Get the last price of coins in JSON data
+//         return new Promise(function (res, rej) {
+//             try {
+//                 for (x in / of data) {
+//                     price = ...;
+//                     coin_prices[coinName][marketName] = price;
+//                 }
+//                 res(coin_prices);
+//             }
+//             catch (err) {
+//                 rej(err);
+//             }
+//
+//         })
+//     },
+//
+//
+// }
+
 let markets = [
 
+    // {
+    //     marketName: 'cryptowatchAPI',
+    //     URL: 'https://api.cryptowat.ch/markets/summaries', //URL To Fetch API From.
+    //     toBTCURL: false, //URL, if needed for an external bitcoin price api.
+    //
+    //     last: function (data, coin_prices, toBTCURL) { //Where to find the last price of coin in JSON data
+    //         return new Promise(function (res, rej) {
+    //             try {
+    //                 data = data.result;
+    //                 for (let key in data) {
+    //                     let marketPair = key.split(':');
+    //                     let market = marketPair[0], pair = marketPair[1];
+    //                     let indexOfBTC = pair.indexOf('btc');
+    //                     if (indexOfBTC > 0 && !pair.includes('future') && !market.includes('qryptos') && !market.includes('quoine') && !market.includes('poloniex')) {
+    //                         if(marketNames.indexOf(market) === -1 ){
+    //                             marketNames.push([[market], ['']]);
+    //                             console.log(marketNames);
+    //                         }
+    //                         let coin = pair.replace(/btc/i, '').toUpperCase();
+    //                         let price = data[key].price.last;
+    //                         if(price > 0) {
+    //                             if (!coin_prices[coin]) coin_prices[coin] = {};
+    //                             coin_prices[coin][market] = price;
+    //
+    //                         }
+    //                     }
+    //                 }
+    //                 res(coin_prices);
+    //             }
+    //             catch (err) {
+    //                 console.log(err);
+    //                 rej(err)
+    //             }
+    //         })
+    //     }
+    //
+    // },
     {
-        marketName: 'bittrex',
-        URL: 'https://api.newdex.io/v1/tickers',
+        marketName: 'newdex',
+        URL: 'https://api.jsonstorage.net/v1/json/ea4562bd-ac24-4518-bfa5-c92e3a7691cc/adccf7c1-3abb-4aa9-b87d-98e91105d181',
         toBTCURL: false,
         pairURL : '',
         last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
             return new Promise(function (res, rej) {
                 try {
-                    for (let obj of data) {
-                        if(obj["currency"].includes('EOS')) {
-                            let coinName = obj["quote_currency"];
+                    for (let obj of data.result) {
+                        if(obj["MarketName"].includes('EOS-')) {
+                            let coinName = obj["MarketName"].replace("EOS-", '');
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].newdex = obj.last;
+                            coin_prices[coinName].newdex = obj.Last;
                         }
                     }
                     res(coin_prices);
@@ -26,20 +88,19 @@ let markets = [
         },
 
     },
-
     {
-        marketName: 'poloniex',
-        URL: 'https://poloniex.com/public?command=returnTicker',
+        marketName: 'defibox',
+        URL: 'https://api.jsonstorage.net/v1/json/ea4562bd-ac24-4518-bfa5-c92e3a7691cc/bdf7d911-05d1-4877-a66a-0f3811f43362',
         toBTCURL: false,
         pairURL : '',
         last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
             return new Promise(function (res, rej) {
                 try {
-                    for (var obj in data) {
-                        if(obj.includes('BTC_')&&obj!=="BTC_EMC2") {
-                            let coinName = obj.replace("BTC_", '');
+                    for (let obj of data.result) {
+                        if(obj["MarketName"].includes('EOS-')) {
+                            let coinName = obj["MarketName"].replace("EOS-", '');
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].poloniex = data[obj].last;
+                            coin_prices[coinName].defibox = obj.Last;
                         }
                     }
                     res(coin_prices);
@@ -53,6 +114,32 @@ let markets = [
         },
 
     },
+    {
+        marketName: 'alcor',
+        URL: 'https://api.jsonstorage.net/v1/json/ea4562bd-ac24-4518-bfa5-c92e3a7691cc/4618b788-8179-434e-beaf-d5e96f2d21f0',
+        toBTCURL: false,
+        pairURL : '',
+        last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
+            return new Promise(function (res, rej) {
+                try {
+                    for (let obj of data.result) {
+                        if(obj["MarketName"].includes('EOS-')) {
+                            let coinName = obj["MarketName"].replace("EOS-", '');
+                            if (!coin_prices[coinName]) coin_prices[coinName] = {};
+                            coin_prices[coinName].alcor = obj.Last;
+                        }
+                    }
+                    res(coin_prices);
+                }
+                catch (err) {
+                    console.log(err);
+                    rej(err);
+                }
+
+            })
+        },
+
+    }
 
 ];
 
