55d3b7ff3c826bd6b5051391ba36116d08502bd6
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2022-05-02 09:27:16 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,8 +7,8 @@ let markets = [
         last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
             return new Promise(function (res, rej) {
                 try {
-                    let data = data.data[*];
-                    for (let obj of data) {
+                   
+                    for (let obj of data.data) {
                         if(obj["currency"].includes('EOS')) {
                             let coinName = obj["quote_currency"].replace('');
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
@@ -17,6 +17,37 @@ let markets = [
                             let coinName = obj["currency"].replace('');
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
                             coin_prices[coinName].newdex = obj.last;
+                        }
+                    }
+                    res(coin_prices);
+                }
+                catch (err) {
+                    console.log(err);
+                    rej(err);
+                }
+
+            })
+        },
+
+    },
+    {
+        marketName: 'alcor',
+        URL: 'https://eos.alcor.exchange/api/markets',
+        toBTCURL: false,
+        pairURL : '',
+        last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
+            return new Promise(function (res, rej) {
+                try {
+                   
+                    for (let obj of data) {
+                        if(obj.base_token.symbol.name.includes('EOS')) {
+                            let coinName = obj.quote_token.symbol.name.replace('');
+                            if (!coin_prices[coinName]) coin_prices[coinName] = {};
+                            coin_prices[coinName].alcor = obj.last_price;
+                        } else {
+                            let coinName = obj.base_token.symbol.name.replace('');
+                            if (!coin_prices[coinName]) coin_prices[coinName] = {};
+                            coin_prices[coinName].alcor = obj.last_price;
                         }
                     }
                     res(coin_prices);
