4ad1cfda0da7a951caaa494358ededd0e23264d0
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2022-05-02 17:11:57 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,22 +1,22 @@
 let markets = [
     {
-        marketName: 'newdex',
-        URL: 'https://api.newdex.io/v1/tickers',
+        marketName: 'defibox',
+        URL: 'https://api.jsonstorage.net/v1/json/ea4562bd-ac24-4518-bfa5-c92e3a7691cc/bdf7d911-05d1-4877-a66a-0f3811f43362',
         toBTCURL: false,
         pairURL : '',
         last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
             return new Promise(function (res, rej) {
                 try {
                    
-                    for (let obj of data.data) {
-                        if(obj["currency"].includes('EOS')) {
-                            let coinName = obj["quote_currency"].replace('');
+                    for (let obj of data) {
+                        if(obj.pair.includes('WAX-')) {
+                            let coinName = obj.pair.replace("WAX-", '');
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].newdex = obj.last;
+                            coin_prices[coinName].defibox = obj.price;
                         } else {
-                            let coinName = obj["currency"].replace('');
+                            let coinName = obj.pair.replace('', "-WAX");
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].newdex = obj.last;
+                            coin_prices[coinName].defibox = obj.price;
                         }
                     }
                     res(coin_prices);
@@ -31,8 +31,8 @@ let markets = [
 
     },
     {
-        marketName: 'alcor',
-        URL: 'https://eos.alcor.exchange/api/markets',
+        marketName: 'alcorswap',
+        URL: 'https://api.jsonstorage.net/v1/json/ea4562bd-ac24-4518-bfa5-c92e3a7691cc/adccf7c1-3abb-4aa9-b87d-98e91105d181',
         toBTCURL: false,
         pairURL : '',
         last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
@@ -40,14 +40,14 @@ let markets = [
                 try {
                    
                     for (let obj of data) {
-                        if(obj.base_token.symbol.name.includes('EOS')) {
-                            let coinName = obj.quote_token.symbol.name.replace('');
+                        if(obj.pair.includes('WAX-')) {
+                            let coinName = obj.pair.replace("WAX-", '');
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].alcor = obj.last_price;
+                            coin_prices[coinName].alcorswap = obj.price;
                         } else {
-                            let coinName = obj.base_token.symbol.name.replace('');
+                            let coinName = obj.pair.replace('', "-WAX");
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].alcor = obj.last_price;
+                            coin_prices[coinName].alcorswap = obj.price;
                         }
                     }
                     res(coin_prices);
