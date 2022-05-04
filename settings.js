fa569452e3e767a2f245213f9e56d57a7d707078
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2022-05-04 21:04:10 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,22 +1,80 @@
+//
+// let boilerPlateMarket =
+// {
+//     marketName: '',
+//     URL: '', //URL To Fetch API From.
+//     toBTCURL: false, //URL, if needed for an external bitcoin price api.
+//     last: function (data, coin_prices) { //Get the last price of coins in JSON data
+//         return new Promise(function (res, rej) {
+//             try {
+//                 for (x in / of data) {
+//                     price = ...;
+//                     coin_prices[coinName][marketName] = price;
+//                 }
+//                 res(coin_prices);
+//             }
+//             catch (err) {
+//                 rej(err);
+//             }
+//
+//         })
+//     },
+//
+//
+// }
+
 let markets = [
+
+    // {
+    //     marketName: 'cryptowatchAPI',
+    //     URL: 'https://api.cryptowat.ch/markets/summaries', //URL To Fetch API From.
+    //     toBTCURL: false, //URL, if needed for an external bitcoin price api.
+    //
+    //     last: function (data, coin_prices, toBTCURL) { //Where to find the last price of coin in JSON data
+    //         return new Promise(function (res, rej) {
+    //             try {
+    //                 data = data.result;
+    //                 for (let key in data) {
+    //                     let marketPair = key.split(':');
+    //                     let market = marketPair[0], pair = marketPair[1];
+    //                     let indexOfBTC = pair.indexOf('btc');
+    //                     if (indexOfBTC > 0 && !pair.includes('future') && !market.includes('qryptos') && !market.includes('quoine') && !market.includes('poloniex')) {
+    //                         if(marketNames.indexOf(market) === -1 ){
+    //                             marketNames.push([[market], ['']]);
+    //                             console.log(marketNames);
+    //                         }
+    //                         let coin = pair.replace(/btc/i, '').toUpperCase();
+    //                         let price = data[key].price.last;
+    //                         if(price > 0) {
+    //                             if (!coin_prices[coin]) coin_prices[coin] = {};
+    //                             coin_prices[coin][market] = price;
+    //
+    //                         }
+    //                     }
+    //                 }
+    //                 res(coin_prices);
+    //             }
+    //             catch (err) {
+    //                 console.log(err);
+    //                 rej(err)
+    //             }
+    //         })
+    //     }
+    //
+    // },
     {
-        marketName: 'defibox',
-        URL: 'https://api.jsonstorage.net/v1/json/ea4562bd-ac24-4518-bfa5-c92e3a7691cc/bdf7d911-05d1-4877-a66a-0f3811f43362',
+        marketName: 'bittrex',
+        URL: 'https://bittrex.com/api/v1.1/public/getmarketsummaries',
         toBTCURL: false,
         pairURL : '',
         last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
             return new Promise(function (res, rej) {
                 try {
-                   
-                    for (let obj of data) {
-                        if(obj.pair.includes('WAX-')) {
-                            let coinName = obj.pair.replace("WAX-", '');
+                    for (let obj of data.result) {
+                        if(obj["MarketName"].includes('BTC-')) {
+                            let coinName = obj["MarketName"].replace("BTC-", '');
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].defibox = obj.price;
-                        } else {
-                            let coinName = obj.pair.replace('', "-WAX");
-                            if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].defibox = obj.price;
+                            coin_prices[coinName].bittrex = obj.Last;
                         }
                     }
                     res(coin_prices);
@@ -30,24 +88,20 @@ let markets = [
         },
 
     },
+
     {
-        marketName: 'alcorswap',
-        URL: 'https://api.jsonstorage.net/v1/json/ea4562bd-ac24-4518-bfa5-c92e3a7691cc/adccf7c1-3abb-4aa9-b87d-98e91105d181',
+        marketName: 'poloniex',
+        URL: 'https://poloniex.com/public?command=returnTicker',
         toBTCURL: false,
         pairURL : '',
         last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
             return new Promise(function (res, rej) {
                 try {
-                   
-                    for (let obj of data) {
-                        if(obj.pair.includes('WAX-')) {
-                            let coinName = obj.pair.replace("WAX-", '');
+                    for (var obj in data) {
+                        if(obj.includes('BTC_')&&obj!=="BTC_EMC2") {
+                            let coinName = obj.replace("BTC_", '');
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].alcorswap = obj.price;
-                        } else {
-                            let coinName = obj.pair.replace('', "-WAX");
-                            if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].alcorswap = obj.price;
+                            coin_prices[coinName].poloniex = data[obj].last;
                         }
                     }
                     res(coin_prices);
@@ -60,7 +114,44 @@ let markets = [
             })
         },
 
-    }
+    },
+	
+	{
+
+        marketName: 'kraken', // kraken has no one size fits all market summery so each pair has to be entered as param in GET - will need to add new coins as they are added to exchange
+        URL: 'https://api.kraken.com/0/public/Ticker?pair=DASHXBT,EOSXBT,GNOXBT,ETCXBT,ETHXBT,ICNXBT,LTCXBT,MLNXBT,REPXBT,XDGXBT,XLMXBT,XMRXBT,XRPXBT,ZECXBT', //URL To Fetch API From.
+        toBTCURL: false, //URL, if needed for an external bitcoin price api.
+        pairURL : '',
+        last: function (data, coin_prices) { //Get the last price of coins in JSON data
+            return new Promise(function (res, rej) {
+                try {
+                    for (let key in data.result) {
+                        let arr = key.match(/DASH|EOS|GNO|ETC|ETH|ICN|LTC|MLN|REP|XDG|XLM|XMR|XRP|ZEC/); // matching real names to weird kraken api coin pairs like "XETCXXBT" etc 
+                        let name = key;
+                        let matchedName = arr[0];
+                        if (matchedName === "XDG") { //kraken calls DOGE "XDG" for whatever reason
+                            let matchedName = "DOGE";
+                            var coinName = matchedName;
+                        } else {
+                            var coinName = matchedName;
+                        }
+
+                        if (!coin_prices[coinName]) coin_prices[coinName] = {};
+                        
+                        coin_prices[coinName].kraken = data.result[name].c[0];
+
+                    }
+                    res(coin_prices);
+
+                }
+                catch (err) {
+                    console.log(err);
+                    rej(err);
+                }
+
+            })
+        },
+    },
 
 ];
 
