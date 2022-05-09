c6e5a30f7280bb66f4203af7e9a6a2f604512b00
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2022-05-09 19:40:02 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,18 +1,23 @@
 let markets = [
 
     {
-        marketName: 'bittrex',
-        URL: 'https://bittrex.com/api/v1.1/public/getmarketsummaries',
+        marketName: 'defibox',
+        URL: 'https://api.jsonstorage.net/v1/json/ea4562bd-ac24-4518-bfa5-c92e3a7691cc/bdf7d911-05d1-4877-a66a-0f3811f43362',
         toBTCURL: false,
         pairURL : '',
         last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
             return new Promise(function (res, rej) {
                 try {
-                    for (let obj of data.result) {
-                        if(obj["MarketName"].includes('BTC-')) {
-                            let coinName = obj["MarketName"].replace("BTC-", '');
+                    for (let obj of data) {
+                        if(obj.sym0.includes('WAX')) {
+                            let coinName = obj.sym1.replace();
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].bittrex = obj.Last;
+                            coin_prices[coinName].bittrex = obj["price1"];
+                        } else if
+                        if(obj.sym1.includes('WAX')) {
+                            let coinName = obj.sym0.replace();
+                            if (!coin_prices[coinName]) coin_prices[coinName] = {};
+                            coin_prices[coinName].bittrex = obj["price2"];
                         }
                     }
                     res(coin_prices);
@@ -28,18 +33,23 @@ let markets = [
     },
 
     {
-        marketName: 'poloniex',
-        URL: 'https://poloniex.com/public?command=returnTicker',
+        marketName: 'alcorswap',
+        URL: 'https://api.jsonstorage.net/v1/json/ea4562bd-ac24-4518-bfa5-c92e3a7691cc/adccf7c1-3abb-4aa9-b87d-98e91105d181',
         toBTCURL: false,
         pairURL : '',
         last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
             return new Promise(function (res, rej) {
                 try {
-                    for (var obj in data) {
-                        if(obj.includes('BTC_')&&obj!=="BTC_EMC2") {
-                            let coinName = obj.replace("BTC_", '');
+                    for (let obj of data) {
+                        if(obj.sym0.includes('WAX')) {
+                            let coinName = obj.sym1.replace();
                             if (!coin_prices[coinName]) coin_prices[coinName] = {};
-                            coin_prices[coinName].poloniex = data[obj].last;
+                            coin_prices[coinName].poloniex = obj["price1"];
+                        }
+                        if(obj.sym1.includes('WAX')) {
+                            let coinName = obj.sym0.replace();
+                            if (!coin_prices[coinName]) coin_prices[coinName] = {};
+                            coin_prices[coinName].bittrex = obj["price2"];
                         }
                     }
                     res(coin_prices);
